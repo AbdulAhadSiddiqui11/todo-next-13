d4124e97a99d9ff6f4cb046fe64ebc80ebc85bd9
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2024-01-27 19:25:20 +0530

    Create Todo form

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,48 @@
+import { prisma } from "@/db";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 import { FC } from "react";
 
-const newPage: FC = () => {
-  return <h1>New page!</h1>;
+const createTodo = async (data: FormData) => {
+  "use server";
+  const title = data.get("title")?.valueOf();
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid Title");
+  }
+
+  await prisma.todo.create({ data: { title, complete: false } });
+  console.log(`Todo created: ${title}`);
+  redirect('/');
 };
 
+const newPage: FC = () => {
+  return (
+    <>
+      <header className="flex justify-between mb-4 items-center">
+        <h1 className="text-2xl">Create a Todo!</h1>
+      </header>
+      <form action={createTodo} className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+        />
+        <div className="flex gap-2 justify-end items-center">
+          <Link
+            href=".."
+            className="border border-white px-3 py-2 rounded-md cursor-pointer hover:bg-indigo-950 focus-within:bg-indigo-950 outline-none"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-white px-3 py-2 rounded-md cursor-pointer hover:bg-indigo-950 focus-within:bg-indigo-950 outline-none"
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
 export default newPage;
